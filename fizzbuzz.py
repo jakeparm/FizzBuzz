510be379fa9ddd9087e5e3b1542d7ecccdb59aa1
Jake Parmentier <jacobparm@gmail.com>
2024-03-08 22:41:18 -0600

    Better readability

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,13 +1,18 @@
 
 #Efficiency is maximized
 def fizzbuzz():
+    #Loop through numbers 1-100
     for i in range(1, 101):
+        #print Fizzbuzz if divisible by both 5 and 3
         if i & 5 == 0 and i & 3 == 0:
             print('FizzBuzz')
+        #print Fizz if divisible by 3
         elif i % 3 == 0:
             print('Fizz')
+        #print Buzz if divisible by 5
         elif i % 5 == 0:
             print('Buzz')
+        #Otherwise print the number
         else:
             print(i)
 
